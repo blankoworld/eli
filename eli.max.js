09cc88d244aeddd76a84fed67d0540eae5e483c1
Olivier DOSSMANN <olivier+git@dossmann.net>
2016-08-05 22:54:40 +0200

    [IMP] New structure with functions and asynchronous calls
      * use functions
      * make asynchronous call to XML files
      * create elements using document.createElement()
      * use querySelector instead of boring childNodes[X].item(0).etc.

--- eli.max.js
+++ eli.max.js
@@ -16,8 +16,8 @@ var user = 'debian';
 var max = 5;
 var tag = 'elitimeline';
 var domain = 'https://quitter.se';
-var location_label = 'Location:';
-var posttime_label = 'Post time:';
+var locationLabel = 'Location:';
+var timeLabel = 'Post time:';
 
 // Network access API url
 var api = '/api/';
@@ -29,7 +29,9 @@ var group_rss = 'statusnet/groups/timeline/';
 // Other variables
 var api_url = "",
     api_rss_url = "",
-    img_name = "";
+    img_name = "",
+    template = "",
+    widget = "";
 
 var setConfig = function setConfig(domain, user, type) {
     api_url = domain + api + user_api;
@@ -45,124 +47,224 @@ var setConfig = function setConfig(domain, user, type) {
 
 var loadConfig = function loadConfig() {
     // fetch user choices
-    var domain = document.getElementById('profile_domain').value;
-    var user = document.getElementById('profile_name').value;
-    var profile_type = document.getElementById('profile_type');
-    var type = profile_type.options[profile_type.selectedIndex].value;
+    domain = document.getElementById('profile_domain').value;
+    user = document.getElementById('profile_name').value;
+    var choosenType = document.getElementById('profile_type');
+    type = choosenType.options[profile_type.selectedIndex].value;
     // change current configuration
     setConfig(domain, user, type);
     // display the result
     displayResult();
 };
 
-var displayResult = function displayResult() {
+var getUrl = function getUrl(url, method) {
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
+        req = new XMLHttpRequest();
     } else {
         // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        req = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.open("GET", url, false);
-    xmlhttp.send();
-    xmlDoc = xmlhttp.responseXML;
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            method(req.responseXML);
+        }
+    });
+    req.send(null);
+};
 
-    userinfo = xmlDoc.getElementsByTagName(type);
-    try {
-        id = userinfo[0].getElementsByTagName("id")[0].childNodes[0].nodeValue;
-    } catch (e) {
-        id = e;
-    }
+var displayResult = function displayResult() {
+    // get main info
+    getUrl(url, createTimeline);
+};
 
-    // fetch image
-    try {
-        img = userinfo[0].getElementsByTagName(img_name)[0].childNodes[0].nodeValue;
-    } catch (e) {
-        img = "";
-    }
-    rss_url = api_rss_url + id + ".xml";
-    xmlhttp.open("GET", rss_url, false);
-    xmlhttp.send();
-    xmlDoc = xmlhttp.responseXML;
-    // Header
-    content = '<div id="eli_widget"><header>';
-    if (img !== '') {
-        content += '<a target= "_blank" href="' + domain + "/" + user + '"><img src="' + img + '" alt="' + user + '" title="Avatar"/></a>';
-    }
-    content += ' <p>' + user + '</p></header>';
+var getTargetStatuses = function getTargetStatuses(source) {
+    // get statuses from source
+    var statuses = source.querySelector('statuses').getElementsByTagName('status');
 
-    // Statuses in Array
-    var items = xmlDoc.getElementsByTagName('statuses').item(0).getElementsByTagName('status');
+    // browse statuses but no more than "max"
     var max_item = max - 1;
+    for (var n = 0; n < statuses.length; n++) {
+        var status = statuses[n];
+        var content = "";
 
-    // Read item from array
-    for (var n = 0; n < items.length; n++) {
-        var item_groupmember = "",
-            item_groupmember_screen_name = "",
-            item_groupmember_name = "",
-            item_groupmember_image = "",
-            item_groupmember_link = "",
-            item_content = "";
+        // Check that this status can be displayed. If status deleted, do not do anything
+        var statusDeleted = "";
+        try {
+            statusDeleted = status.getElementsByTagName('qvitter_delete_notice').item(0).firstChild.data;
+        } catch (e) {}
 
-        // Images, names and link for Group member
-        if (type === "group") {
+        // Check status and add it to timeline only if it was not deleted
+        if (statusDeleted !== 'true') {
+            // Add a bloc about member that contains images, names and link
+            var memberInfo = createMemberInfoBloc(status);
+
+            // get status real content (in html)
             try {
-                item_groupmember_screen_name = "@" + items[n].getElementsByTagName('screen_name').item(0).firstChild.data;
-                item_groupmember_name = items[n].getElementsByTagName('name').item(0).firstChild.data;
-                item_groupmember_image = items[n].getElementsByTagName('profile_image_url_https').item(0).firstChild.data;
-                item_groupmember_link = items[n].getElementsByTagNameNS('*', 'profile_url').item(0).firstChild.data;
-                item_groupmember = '<span class=\"groupmember\"><a class=\"tooltip\" target= \"_blank\" href=\"' + item_groupmember_link + '\"><img alt=\"Profile\" align=\"left\" src="' + item_groupmember_image + '\" />' + item_groupmember_screen_name + ' <span>' + item_groupmember_name + '</span></a></span>';
+                content = status.getElementsByTagNameNS('*', 'html').item(0).firstChild.data;
+                content = content.replace(content.slice(content.indexOf('<div'), content.indexOf('div>') + 4), '');
             } catch (e) {}
-        }
 
-        // Get Status Html
-        try {
-            item_content = items[n].getElementsByTagNameNS('*', 'html').item(0).firstChild.data;
-            item_content = item_content.replace(item_content.slice(item_content.indexOf('<div'), item_content.indexOf('div>') + 4), '');
-        } catch (e) {}
+            // If some picture with a link, get it.
+            var imageElt = "";
+            try {
+                var enclosure = status.querySelector('attachments enclosure');
+                if (['image/jpeg', 'image/gif', 'image/png', 'image/svg'].indexOf(enclosure.getAttribute("mimetype")) >= 0) {
+                    var imageUrl = enclosure.getAttribute("url");
+                    imageElt = document.createElement("a");
+                    imageElt.target = "_blank";
+                    imageElt.href = imageUrl;
+                    var image = document.createElement("img");
+                    image.src = imageUrl;
+                    image.alt = "Attachment";
+                    imageElt.appendChild(image);
+                }
+            } catch (e) {}
 
-        //Get image and link if attached
-        var image_url = "",
-            image_link = "";
-        try {
-            if (['image/jpeg', 'image/gif', 'image/png', 'image/svg'].indexOf(items[n].getElementsByTagName('attachments').item(0).getElementsByTagName('enclosure').item(0).getAttribute("mimetype")) >= 0) {
-                image_url = items[n].getElementsByTagName('attachments').item(0).getElementsByTagName('enclosure').item(0).getAttribute("url");
-                image_link = "<a target= \"_blank\" href=\"" + image_url + "\"><img alt=\"Attachment\" src=" + image_url + " /> </a>";
+            // Status created date
+            var timeElt = "";
+            try {
+                var time = new Date(status.querySelector('created_at').textContent).toLocaleString();
+                timeElt = document.createElement("span");
+                timeElt.className = "tooltip";
+                var labelElt = document.createElement("span");
+                labelElt.appendChild(document.createTextNode(timeLabel + " " + time));
+                timeElt.appendChild(document.createTextNode(" ⌛ "));
+                timeElt.appendChild(labelElt);
+            } catch (e) {}
+
+            // Status location
+            var locationElt = "";
+            try {
+                var location = status.querySelector('user location').textContent;
+                locationElt = document.createElement("span");
+                locationElt.className = "tooltip";
+                locationElt.appendChild(document.createTextNode(" ⌂ "));
+                var labelLocationElt = document.createElement("span");
+                labelLocationElt.textContent = locationLabel + " " + location;
+                locationElt.appendChild(labelLocationElt);
+            } catch (e) {}
+
+            // Add status on timeline
+            var item = document.createElement("article");
+            var htmlContent = "";
+            if (memberInfo !== "") {
+                htmlContent += "<span>" + memberInfo.innerHTML + "</span>";
             }
-        } catch (e) {}
-
-        //Get Create Time from status
-        var item_time = "";
-        try {
-            item_time = new Date(items[n].getElementsByTagName('created_at').item(0).firstChild.data).toLocaleString();
-            item_time = "<span class=\"tooltip\">&#x231B\;<span>" + posttime_label + " " + item_time + "</span> </span>";
-        } catch (e) {}
-
-        //Get location from status
-        var item_location = "";
-        try {
-            item_location = items[n].getElementsByTagName('user').item(0).getElementsByTagName('location').item(0).firstChild.data;
-            item_location = "<span class=\"tooltip\">&#x2302\; <span>" + location_label + " " + item_location + "</span></span>";
-        } catch (e) {}
-
-        //Check for delete notice
-        var delete_notice = "";
-        try {
-            delete_notice = items[n].getElementsByTagName('qvitter_delete_notice').item(0).firstChild.data;
-        } catch (e) {}
-
-        //Build status string and add to timeline
-        if (delete_notice !== 'true') {
-            content += '<article>' + item_groupmember + item_time + item_location + "<br />" + item_content + image_link + '</article>';
+            if (timeElt !== "") {
+                htmlContent += "<span class=\"tooltip\">" + timeElt.innerHTML + "</span>";
+            }
+            if (locationElt !== "") {
+                htmlContent += "<span class=\"tooltip\">" + locationElt.innerHTML + "</span>";
+            }
+            htmlContent += document.createElement("br").innerHTML;
+            htmlContent += content;
+            if (imageElt !== "") {
+                htmlContent += imageElt.innerHTML;
+            }
+            htmlContent += "";
+            item.innerHTML = htmlContent;
+            widget.insertBefore(item, templateFooter);
         }
 
-        if ((max_item < items.length) && (n == max_item)) {
-            n = items.length;
+        // Hack to break the loop
+        if ((max_item < statuses.length) && (n === max_item)) {
+            n = statuses.length;
         }
     }
+};
 
-    content += '<footer></footer></div>';
-    document.getElementById(tag).innerHTML = content;
+var createTemplate = function createTemplate(image) {
+    // Initialization
+    template = document.createElement("div");
+    template.id = tag;
+    widget = document.createElement("div");
+    widget.id = "eli_widget";
+    // Add header with the username and a link to its social network
+    var templateHeader = document.createElement("header");
+    if (image !== "") {
+        var networkLink = document.createElement("a");
+        networkLink.target = "_blank";
+        networkLink.href = domain + "/" + user;
+        var targetImage = document.createElement("img");
+        targetImage.src = image;
+        targetImage.title = "Avatar";
+        targetImage.alt = user;
+        networkLink.appendChild(targetImage);
+        templateHeader.appendChild(networkLink);
+    }
+    var content = document.createElement("p");
+    content.textContent = "\u00A0" + user;  // add a space before target
+    templateHeader.appendChild(content);
+    widget.appendChild(templateHeader);
+    // Finally add footer
+    templateFooter = document.createElement("footer");
+    widget.appendChild(templateFooter);
+};
+
+var createTimeline = function createTimeline(source) {
+    // get info about target
+    var targetInfo = source.getElementsByTagName(type);
+    // get image source
+    var imageSource = "";
+    try {
+        imageSource = source.querySelector(img_name).textContent;
+    } catch (e) {}
+
+    // get target ID to fetch RSS feed (for an example with ID 3, RSS feed is 3.xml)
+    try {
+        targetId = source.querySelector("id").textContent;
+    } catch (e) {
+        throw new Error(alert("Can't fetch social network info."));
+    }
+
+    // create template
+    createTemplate(imageSource);
+
+    // fetch RSS feed
+    rss_url = api_rss_url + targetId + ".xml";
+    getUrl(rss_url, getTargetStatuses);
+
+    // replace timeline by new content
+    template.appendChild(widget);
+    var timeline = document.getElementById(tag);
+    parent = timeline.parentNode;
+    parent.replaceChild(template, timeline);
+};
+
+var createMemberInfoBloc = function createMemberInfoBloc(text) {
+    // only create this bloc if type is group
+    var infoBloc = "";
+    if (type === "group") {
+        try {
+            // get data
+            var pseudo = "@" + text.querySelector('screen_name').textContent;
+            var name = text.querySelector('name').textContent;
+            var image = text.querySelector('profile_image_url_https').textContent;
+            var link = text.getElementsByTagNameNS('*', 'profile_url').item(0).firstChild.data;
+            infoBloc = document.createElement("span");
+            // then start creating bloc
+            infoBloc.className = "groupmember";
+            var linkElt = document.createElement("a");
+            linkElt.className = "tooltip";
+            linkElt.target = "_blank";
+            linkElt.href = link;
+            // add image, pseudo, then name into the link
+            var imageElt = document.createElement("img");
+            imageElt.alt = "Profile";
+            imageElt.style.align = "left"; // TODO: Add this directive into CSS
+            imageElt.src = image;
+            linkElt.appendChild(imageElt);
+            linkElt.appendChild(document.createTextNode(pseudo));
+            var nameElt = document.createElement("span");
+            nameElt.textContent = name;
+            linkElt.appendChild(nameElt);
+            // finally add link to the bloc
+            infoBloc.appendChild(linkElt);
+        } catch (e) {}
+    }
+    return infoBloc;
 };
 
 // Set default configuration
